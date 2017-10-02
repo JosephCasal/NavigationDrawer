ccbd491f9fb53af9ecb1fe18675655a729297151
Joseph Casal <joseph@Josephs-MacBook-Air.local>
2017-10-02 13:16:13 -0400

    upload project

---
+++ app/src/main/java/com/example/joseph/weekend1/PictureActivity.java
@@ -0,0 +1,52 @@
+package com.example.joseph.weekend1;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class PictureActivity extends AppCompatActivity {
+
+    private ImageView iv;
+    private Bitmap imageBitmap;
+    private Button b;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        iv = (ImageView) findViewById(R.id.ivPhoto);
+        b = (Button) findViewById(R.id.btnTakePicture);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            iv.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+}
